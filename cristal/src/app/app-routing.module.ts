11631bad63ddb26461fb9ddc977256c5f5b54043
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-04-29 10:56:22 -0300

    Criação das rotas

--- cristal/src/app/app-routing.module.ts
+++ cristal/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FaqComponent } from './faq/faq.component';
+import { LoginComponent } from './login/login.component';
+import { PoliticaDePrivacidadeComponent } from './politica-de-privacidade/politica-de-privacidade.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'politica-de-privacidade', component: PoliticaDePrivacidadeComponent},
+  {path: 'produtos', component: ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
