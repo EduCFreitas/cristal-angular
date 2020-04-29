ac0c3815cab9ff5d945a79a51e414eaad5efbbe9
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-04-29 00:12:46 -0300

    Criação do projeto Angular, criação dos componentes iniciais

---
+++ cristal/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatoComponent } from './contato/contato.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PoliticaDePrivacidadeComponent } from './politica-de-privacidade/politica-de-privacidade.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CadastroComponent,
+    ContatoComponent,
+    FaqComponent,
+    HomeComponent,
+    LoginComponent,
+    PoliticaDePrivacidadeComponent,
+    ProdutosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
