c49dbdbc55a59910c5bd2821ef92d61f862aaccc
José Eduardo Candido de Freitas <jeduardocfreitas@gmail.com>
2020-06-04 18:49:41 -0300

    Início da implementação da validação do formulário do componente Contato

--- cristal/src/app/contato/contato.component.ts
+++ cristal/src/app/contato/contato.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
+  nome:string = ''
+  email:string = '';
+  telefone:string = '';
+  numTelefone:number;
+  erroTelefone:boolean = false;
+
+  i:number = 0;
+
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  validarTelefone(){
+    this.numTelefone = parseInt(this.telefone);
+    if(this.telefone.length<10 || this.telefone.length>11 || isNaN(this.numTelefone)){
+      this.erroTelefone = true;
+    }else{
+      this.erroTelefone = false;
+    }
+    console.log(this.telefone)
+    console.log(this.numTelefone)
+  }
+
+  enviar(){
+
+  }
+
+  
 
 }
