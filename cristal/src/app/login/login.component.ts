30fd13a0b43233cea1032b921b79f53cf179d68e
Solly <sollyson_rodrigues@hotmail.com>
2020-06-18 19:09:11 -0300

    Edição da pagina contato

--- cristal/src/app/login/login.component.ts
+++ cristal/src/app/login/login.component.ts
@@ -10,6 +10,28 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
   }
 
 }
