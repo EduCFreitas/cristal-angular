5d55365a7d046a858bbd7588454a73909686c85b
solly42 <sollyson_rodrigues@hotmail.com>
2020-06-25 20:30:20 -0300

    Novo navbar

--- cristal/src/app/navbar/navbar.component.ts
+++ cristal/src/app/navbar/navbar.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+declare var $: any;
 
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
-})
-export class NavbarComponent implements OnInit {
+@Component( {
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
+  }
 
-  constructor() { }
+) export class NavbarComponent implements OnInit {
+
+  constructor() {}
 
   ngOnInit(): void {
-    
+
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 75) {
+        $(".black").css("background" , "rgba(255, 255, 255, 0.527)");
+      }
+
+      else{
+        $(".black").css("background" , "#edf0f100");
+        }
+      })
+    })
+
+    const doc=document;
+    const menuOpen=doc.querySelector(".menu");
+    const menuClose=doc.querySelector(".close");
+    const overlay=doc.querySelector(".overlay");
+
+    menuOpen.addEventListener("click", ()=> {
+        overlay.classList.add("overlay--active");
+      }
+
+    );
+
+    menuClose.addEventListener("click", ()=> {
+        overlay.classList.remove("overlay--active");
+      }
+
+    );
+
+
   }
 
 }
+
+
