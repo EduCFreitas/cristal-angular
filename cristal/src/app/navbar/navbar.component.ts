da7b1015476cf6aef21097ef7b55a03b8f710710
solly42 <sollyson_rodrigues@hotmail.com>
2020-06-27 14:00:47 -0300

    Alterações contato e Cadastro

--- cristal/src/app/navbar/navbar.component.ts
+++ cristal/src/app/navbar/navbar.component.ts
@@ -9,55 +9,55 @@ declare var $: any;
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  
+
   nomeUsuario:string;
   tipoUsuario:string;
   admin:boolean;
-  
+
   constructor(private router:Router, public auth:AuthService) { }
-  
+
   ngOnInit(): void {
-    
+
     $(document).ready(function(){
       $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 170) {
-          $(".black").css("background" , "rgba(51, 51, 51, 0.657)");
+          $(".black").css("background" , "#3b0e67a8");
         }
-        
+
         else{
           $(".black").css("background" , "#edf0f100");
         }
       })
     })
-    
+
     const doc=document;
     const menuOpen=doc.querySelector(".menu");
     const menuClose=doc.querySelector(".close");
     const overlay=doc.querySelector(".overlay");
-    
-    
+
+
     menuOpen.addEventListener("click", ()=> {
       overlay.classList.add("overlay--active");
-      
+
     }
-    
+
     );
-    
+
     menuClose.addEventListener("click", ()=> {
       overlay.classList.remove("overlay--active");
     }
-    
+
     );
-    
+
     overlay.addEventListener("click", ()=> {
       overlay.classList.remove("overlay--active");
     }
-    
+
     );
-    
+
   }
-  
+
   atualizaNavbar(){
     this.nomeUsuario = sessionStorage.getItem('nomeUsuario');
     this.tipoUsuario = sessionStorage.getItem('tipoUsuario');
@@ -69,17 +69,17 @@ export class NavbarComponent implements OnInit {
     }
     this.separaPrimeiroNome(this.nomeUsuario);
   }
-  
+
   separaPrimeiroNome(nomeUsuario){
     let primeiroNome = this.nomeUsuario.split(' ').slice(0,1);
     return primeiroNome;
   }
-  
+
   sair(){
     this.router.navigate(['/home'])
     sessionStorage.clear();
   }
-  
+
 }
 
 
